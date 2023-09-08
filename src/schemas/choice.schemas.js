65c8b28f47c408ed0324510c81cccdcaa3ba6f86
Jorge Neto <jorge.c.neto@hotmail.com>
2023-09-08 15:33:08 -0300

    retirada do unique schema

--- src/schemas/choice.schemas.js
+++ src/schemas/choice.schemas.js
@@ -1,5 +1,5 @@
 import Joi from "joi"
 
-export const choiceSchema =Joi.object({
-    title: Joi.string().required().unique()
+export const choiceSchema = Joi.object({
+    title: Joi.string().required()
 })
