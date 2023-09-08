7d793bcd71d3f75c031995b8fe50c2230b6e6be2
Jorge Neto <jorge.c.neto@hotmail.com>
2023-09-08 03:41:13 -0300

    post choice

--- src/controllers/choice.controllers.js
+++ src/controllers/choice.controllers.js
@@ -1,11 +1,18 @@
+import { ObjectId } from "mongodb"
 import { db } from "../database/database.connection.js"
 
 export async function choice(req, res) {
     const { title } = req.body
-    const { id } = req.params
+    const { id } =req.params
 
     try {
-        
+        const verificarChoice = await db.collection("choice").findOne({title})
+
+        if(verificarChoice){
+            return res.status(409).send("Título já utilizado")
+        }
+        await db.collection("choice").insertOne({ title, pollId: ObjectId(id) })
+        return res.sendStatus(201)
     } catch {
         return res.status(500).send(err.message)
     }
